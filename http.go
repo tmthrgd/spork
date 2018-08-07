671707ba4660254f1f06abfa6bb1ffeaf8c5d9b0
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-08-07 12:15:29 +0930

    Use a HTML error page for 500 errors

--- http.go
+++ http.go
@@ -1,6 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+var error500 = template.Must(template.New("error500").Parse(`<!doctype html>
+<meta charset=utf-8>
+<title>500 Internal Server Error</title>
+<style>body{margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0 10px}h1,h2,h3{line-height:1.2}</style>
+<h1>500 Internal Server Error</h1>
+<p>{{.}}</p>`))
 
 func httpHandlerError(handler func(http.ResponseWriter, *http.Request) error) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -9,6 +19,9 @@ func httpHandlerError(handler func(http.ResponseWriter, *http.Request) error) ht
 			return
 		}
 
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusInternalServerError)
+
+		error500.Execute(w, err.Error())
 	}
 }
