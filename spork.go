11c354c83d6a9fc786719866ae05d64a234daa37
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-08-07 11:57:39 +0930

    Replace utility with http server

--- spork.go
+++ spork.go
@@ -2,51 +2,83 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/tmthrgd/httphandlers"
 	"github.com/tmthrgd/spork/internal/dbus"
 )
 
+var (
+	favicon = "\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x10\x00\x00\x00\x10\b\x06\x00\x00\x00\x1f\xf3\xffa\x00\x00\x00\x16IDATx\xdacd\xa0\x100\x8e\x1a0j\xc0\xa8\x01\xc3\xc5\x00\x00&\x98\x00\x11\x9b\x92AZ\x00\x00\x00\x00IEND\xaeB`\x82"
+	robots  = "User-agent: *\nDisallow: /"
+)
+
+var error404 = `<!doctype html>
+<meta charset=utf-8>
+<title>404 Not Found</title>
+<style>body{margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0 10px}h1,h2,h3{line-height:1.2}</style>
+<h1>404 Not Found</h1>
+<p>The requested file was not found.</p>`
+
 func main() {
+	port := flag.Int("port", 8080, "the port to listen on")
+	flag.Parse()
+
 	bus, err := dbus.SessionBus()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer bus.Close()
 
-	ctx, cancel := context.WithCancel(context.Background())
+	notFoundHandler := handlers.ServeError(http.StatusNotFound, []byte(error404), "text/html; charset=utf-8")
+
+	router := chi.NewRouter()
+	router.Use(middleware.GetHead)
+	router.NotFound(notFoundHandler.ServeHTTP)
+
+	now := time.Now()
+	router.Get("/favicon.ico", handlers.ServeString("favicon.png", now, favicon).ServeHTTP)
+	router.Get("/robots.txt", handlers.ServeString("robots.txt", now, robots).ServeHTTP)
+
+	handler := handlers.AccessLog(router, nil)
+	handler = &handlers.SecurityHeaders{
+		Handler: router,
+	}
+	handler = handlers.SetHeader(handler, "Server", "spork (audacious control panel)")
+
+	fmt.Printf("Listening on :%d\n", *port)
+
+	srv := &http.Server{
+		Addr:    ":" + strconv.Itoa(*port),
+		Handler: handler,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	// termination handler
+	term := make(chan os.Signal, 1)
+	signal.Notify(term, os.Interrupt, syscall.SIGTERM)
+	<-term
+
+	// gracefull shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	entries, err := bus.GetPlaylistLength(ctx)
-	if err != nil {
-		log.Fatal(err)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("error shutting down: %v", err)
 	}
-
-	plural := "s"
-	if entries == 1 {
-		plural = ""
-	}
-
-	fmt.Printf("%d track%s.\n", entries, plural)
-
-	var total uint64
-
-	for entry := uint32(0); entry < uint32(entries); entry++ {
-		title, err := bus.GetSongTitle(ctx, entry)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		length, err := bus.GetSongLength(ctx, entry)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		total += uint64(length)
-
-		fmt.Printf("%4d | %-60s | %d:%02d\n", entry+1, title, length/60, length%60)
-	}
-
-	fmt.Printf("Total length: %d:%02d\n", total/60, total%60)
 }
