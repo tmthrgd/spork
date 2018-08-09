41e9d3cb686bb1b9bc439a00e2ee8114c460746d
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-08-09 17:30:04 +0930

    Display the currently playing song on the controls page

--- controls.go
+++ controls.go
@@ -14,7 +14,8 @@ var controlTmpl = newTemplate(`<!doctype html>
 <h1>Play Controls</h1>
 <div class=controls>
 <a href=/controls/prev>⏮</a> <a href=/controls/stop>⏹</a> <a href=/controls/playpause>⏯</a> <a href=/controls/next>⏭</a><br>
-<input type=range min=0 max=100 value="{{.Volume}}" title="{{.Volume}}%" class=volume><br>
+<input type=range min=0 max=100 value="{{.Volume}}" title="{{.Volume}}%" class=volume>
+<p class=song>{{if .Title}}{{.Title}} ({{FormatLength .Length}}){{end}}</p>
 <span class=error></span>
 </div>
 <a href=/playlist>Active Playlist</a> – <a href=/playlist#current>Current Song</a>
@@ -23,11 +24,32 @@ var controlTmpl = newTemplate(`<!doctype html>
 
 func controlsHandler() http.HandlerFunc {
 	return errorHandler(func(w http.ResponseWriter, r *http.Request) error {
-		volume, err := dbus.GetVolume(r.Context())
+		ctx := r.Context()
+
+		volume, err := dbus.GetVolume(ctx)
 		if err != nil {
 			return err
 		}
 
-		return templateExecute(w, controlTmpl, &struct{ Volume int32 }{volume})
+		pos, err := dbus.GetPlaylistPosition(ctx)
+		if err != nil {
+			return err
+		}
+
+		title, err := dbus.GetSongTitle(ctx, pos)
+		if err != nil {
+			return err
+		}
+
+		length, err := dbus.GetSongLength(ctx, pos)
+		if err != nil {
+			return err
+		}
+
+		return templateExecute(w, controlTmpl, &struct {
+			Volume int32
+			Title  string
+			Length int32
+		}{volume, title, length})
 	})
 }
