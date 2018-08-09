e1e29c0d1f676c6750f86a687f7f3b2f207aa75e
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-08-09 17:39:52 +0930

    Update the currently playing song on the control page

--- assets/controls.js
+++ assets/controls.js
@@ -13,3 +13,18 @@ document.querySelector('.volume').addEventListener('input', e => {
 	fetch(`/controls/volume/${encodeURIComponent(e.target.value)}`, {headers})
 		.then(fetchThen, fetchCatch);
 }, false);
+
+const song = document.querySelector('.song');
+
+const es = new EventSource('/playlist/updates');
+
+es.onmessage = msg => {
+	const {title, length} = JSON.parse(msg.data);
+	if (title) {
+		song.textContent = `${title} (${(length/60)|0}:${('0'+((length%60)|0)).slice(-2)})`;
+	}
+};
+
+es.onerror = e => {
+	console.error(e);
+};
